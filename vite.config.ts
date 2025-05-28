b5e6e085219a62b77d2b44da135becc6e004e9c3
DAVIDLOPEZSOMACYL <david.lopez@somacyl.es>
2025-05-28 14:22:29 +0000

    comentario que se quiera

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
  plugins: [react()],
- base: '/retechfor/', // Ejemplo: '/mi-proyecto-react/'
+ base: '/davidlopezsomacyl.github.io/', // Ejemplo: '/mi-proyecto-react/'
 })
